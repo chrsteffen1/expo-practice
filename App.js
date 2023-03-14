9d57f874fec9d4ae6d1a0dfdb53006a0f7d5d96c
Chrsteffen <Chrsteffen1@gmail.com>
2023-03-14 14:25:17 -0400

    add button counter

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import React,{useState} from 'react';
+import { Text, View, ScrollView, Button } from 'react-native';
 
-const App = () => (
+const App = () => {
+  const [pressedCount, setPressedCount] = useState(0)
+
+  return(
   <View style={{ flex: 1, justifyContent: 'center' }}>
     <Text style={{ fontSize: 24, textAlign: 'center' }}>
       Scroll me!
@@ -14,8 +17,19 @@ const App = () => (
         <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
       </ScrollView>
     </View>
-  </View>
-);
+    <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : 'The button isn\'t pressed yet'
+        }
+      </Text>
+      <Button
+        title='Press me'
+        onPress={() => setPressedCount(pressedCount + 1)}
+      />
+  </View> 
+  )
+};
 
 export default App;
 
